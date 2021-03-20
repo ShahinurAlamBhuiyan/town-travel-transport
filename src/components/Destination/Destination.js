eb33118ce100134d1ae5028c28a5717ee93c740e
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-03-20 21:25:34 +0600

    last touch

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,28 +1,27 @@
 import React, { useState } from 'react';
 import Header from '../Header/Header';
-import { useHistory, useParams } from 'react-router';
-import { Link } from 'react-router-dom';
 import GoogleMap from '../GoogleMap/GoogleMap';
 const Destination = () => {
-    const { destination } = useParams();
-    const history = useHistory();
-    const [showPackages, setShowPackages] = useState(false)
     const [location, setLocation] = useState({ from: null, to: null })
     const onBlurHandler = (e) => {
         const key = e.target.name
         setLocation({ ...location, [key]: e.target.value })
+    }
+
+    const handleSubmit =(e) => {
+        e.preventDefault()
     }
     return (
         <>
             <Header />
                 <div className="container row mt-5">
                     <div className="col-md-5">
-                         <form onSubmit={() => setShowPackages(true)} className="form my-4">
+                         <form onSubmit={handleSubmit} className="form my-4">
                                 <div className="mb-3">
-                                    <input name="from" onBlur={(e) => onBlurHandler(e)} type="text" placeholder="from" className="form-control" required />
+                                    <input name="from" type="text" placeholder="from" className="form-control" required />
                                 </div>
                                 <div className="mb-3">
-                                    <input name="to" onBlur={(e) => onBlurHandler(e)} type="text" placeholder="to" className="form-control" required />
+                                    <input name="to" type="text" placeholder="to" className="form-control" required />
                                 </div>
                                 <div className="mb-3">
                                     <input type="date" className="form-control" required />
@@ -30,7 +29,7 @@ const Destination = () => {
                                 <div className="mb-3">
                                     <input type="time" className="form-control" required />
                                 </div>
-                                <button className='btn btn-primary w-100' type='submit'> Search</button>
+                                <button  className='btn btn-primary w-100'type="submit"> Search</button>
                             </form>
                         <div>
                         </div>
